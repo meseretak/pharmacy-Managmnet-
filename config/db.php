89f1b58028cdf10c4e81d3d54e582f34f2d813ed
meseret <meseretinsa@gmail.com>
2026-04-19 00:58:11 +0300

    added

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
-define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '3301');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'pharmacy_mgmt');
+define('DB_HOST', getenv('DB_HOST') ?: '127.0.0.1');
+define('DB_PORT', getenv('DB_PORT') ?: '3306');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') ?: '');
+define('DB_NAME', getenv('DB_NAME') ?: 'pharmacy_mgmt');
 define('APP_NAME', 'PharmaCare Pro');
 define('APP_VERSION', '1.0.0');
 define('CURRENCY', 'ETB');
